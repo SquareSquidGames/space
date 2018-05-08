967364759eb3ee8ca6e676bce41045d99b230046
Unknown <JonathanILevi@gmail.com>
2018-05-08 18:41:56 -0500

    GL does it's thing.  (yeah bad commit msg...)

--- source/io/renderer.d
+++ source/io/renderer.d
@@ -8,6 +8,7 @@ import glwt.vertex_array;
 import glwt.draw;
 import glwt.shader;
 import glwt.program;
+import glwt.viewport;
 import gl_context;
 
 import llist.slist;
@@ -17,29 +18,39 @@ import game_events.game_event;
 private {
 	enum string vertexShader = 
 		"#version 330
+		
+		uniform mat4 projection;
+		uniform vec2 pos = vec2(0,0);
 
+		layout (location = 0) in vec3 inPos;
+		layout (location = 1) in vec3 inColor;
 
-		//uniform vec3 pos = vec3(0,0,0);
-
-		layout (location = 0) in vec3 inPosition;
-		//layout (location = 1) in vec3 inColor;
-
-		//smooth out vec3 theColor;
+		smooth out vec3 vertColor;
 
 		void main() {
-			gl_Position = vec4(inPosition, 1.0);
-			//theColor = inColor;
+			////gl_Position = vec4(inPos, 1.0);
+			////gl_Position = vec4(inPos.xy+pos,inPos.z, 1.0);
+			gl_Position = projection*vec4(inPos.xy+pos,inPos.z, 1.0);
+			vertColor = inColor;
 		}";
 
 	enum string fragmentShader =
 		"#version 330
 
-		//smooth in vec3 theColor;
+		smooth in vec3 vertColor;
 		out vec4 outputColor;
 
 		void main() {
-			outputColor = vec4(1,0,0, 1.0);
+			outputColor = vec4(vertColor, 1.0);
 		}";
+}
+
+
+float[4][4] orthoProjection(float[2] size, float nearPlane, float farPlane) {
+	return [	[1f/size[0]	,0f	,0f	,0f	],
+		[0f	,1f/size[1]	,0f	,0f	],
+		[0f	,0f	,-2f/(farPlane-nearPlane)	,0f	],
+		[0f	,0f	,-(farPlane+nearPlane)/(farPlane-nearPlane)	,1f	]];
 }
 
 
@@ -52,8 +63,12 @@ class Renderer {
 	VertexShader vert;
 	FragmentShader frag;
 	Program program;
+	int uniformLoc_projection;
+	int uniformLoc_pos;
+	float[4][4] projection;
 
 	ArrayBuffer objectVerts;
+	ArrayBuffer objectCols;
 	VertexArray object;
 
 	this(World world) {
@@ -61,7 +76,10 @@ class Renderer {
 
 		SetupGl.ver(3,2);
 		SetupGl.ContextProfileMask.core;
-		window = new Window("Window!!!", [256,192], Window.Flags.OPENGL);
+		int x = 256;
+		int y = 192;
+		window = new Window("Window!!!", [x,y], Window.Flags.OPENGL|Window.Flags.Resizable);
+		calcProjection([x,y]);
 		
 		context = window.createGLContext;
 		reloadGl();
@@ -78,25 +96,50 @@ class Renderer {
 		program.attach(frag);
 		program.link;
 		
+		program.use;
+		uniformLoc_projection	= program.getUniform("projection");
+		uniformLoc_pos	= program.getUniform("pos");
+		
 
 
 		float[3][] vertices = [
-			[	-0.5	, -0.5	, 0.0f	]	,
-			[	0.5	, -0.5	, 0.0f	]	,
-			[	0.0	, 0.5	, 0.0f	]	,
+			[	-0.1	, -0.1	, 0.0f	]	,
+			[	0.1	, -0.1	, 0.0f	]	,
+			[	0.0	, 0.1	, 0.0f	]	,
+		];
+		float[3][] colors = [
+			[	1	, 0	, 0	]	,
+			[	0	, 1	, 0	]	,
+			[	0	, 0	, 1	]	,
 		];
 		object.gen;
 		object.bind;
 		object.enableAttribute(0);
+		object.enableAttribute(1);
 		objectVerts.gen;
 		objectVerts.bind;
 		objectVerts.data(vertices, BufferUsage.StaticDraw);
 		object.attributePointer(0, objectVerts, 3, DataType.Float);
-		
+		objectCols.gen;
+		objectCols.bind;
+		objectCols.data(colors, BufferUsage.StaticDraw);
+		object.attributePointer(1, objectCols, 3, DataType.Float);
+	}
+	
+	void resize(int[2] size) {
+		window.resize(size);
+		setGlViewport([0,0],size);
+		calcProjection(size);
+	}
+	void calcProjection(int[2] size) {
+		projection = orthoProjection([size[0]>size[1]?cast(float)size[0]/size[1]:1,size[0]<size[1]?cast(float)size[1]/size[0]:1],-1,1);
 	}
 
 	void render() {
 		program.use;
+		
+		program.uniformMatrix!(float,4,4)(uniformLoc_projection, projection);
+		program.uniformVector(uniformLoc_pos, [0f,0f]);
 		
 		object.bind;
 		object.draw(Primitive.Triangles, 3);
