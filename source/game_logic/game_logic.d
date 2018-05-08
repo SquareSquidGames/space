08bf739694a0475b5029b3da186ccec20897d34b
Unknown <agentultravoid@gmail.com>
2018-05-07 19:32:07 -0500

    Added game_event handling ot game logic update

--- source/game_logic/game_logic.d
+++ source/game_logic/game_logic.d
@@ -1,6 +1,7 @@
 module game_logic.game_logic;
 import llist.slist;
 import objects.wyrm;
+import game_events.game_event;
 
 class GameLogic {
 	Node!Wyrm* wyrms;
@@ -9,9 +10,24 @@ class GameLogic {
 		wyrms.append(new Wyrm);
 	}
 	
-	void update() {
-		foreach (wyrm; wyrms.iterator) {
-			wyrm.value.pos[0]++;
+	void update(GameEvent[] gameEvents) {
+		foreach (event; gameEvents) {
+			if (event.type == EventType.wyrmRotate) {
+				Node!Wyrm* wyrmNode = wyrms;
+				foreach (_; 0..event.wyrmRotate.playerNum) {
+					wyrmNode = wyrmNode.next;
+				}
+				wyrmNode.value.rot += event.wyrmRotate.torque;
+			}
+			else if (event.type == EventType.wyrmThrust) {
+				
+			}
+			else if (event.type == EventType.wyrmWarp) {
+				
+			}
+		}
+		foreach (wyrmNode; wyrms.iterator) {
+			////wyrmNode.value.pos[0]++;
 		}
 	}
 }
