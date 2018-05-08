1c07ac960ee7eebda40bdca7036bfd2e5dd5b767
Unknown <JonathanILevi@gmail.com>
2018-05-07 23:47:14 -0500

    New ioManager

--- source/io/io_manager.d
+++ source/io/io_manager.d
@@ -1,26 +1,28 @@
 module io.io_manager;
+
 import llist.slist;
-import objects.wyrm;
+import objects.world;
 import game_events.game_event;
 
 class IoManager {
-	Node!Wyrm* wyrms;
-	this(Node!Wyrm* wyrms) {
-		this.wyrms = wyrms;
+	World world;
+	
+	this(World world) {
+		this.world = world;
 	}
 	
 	void render() {
-		foreach (wyrmNode; wyrms.iterator) {
-			import std.stdio;
-			wyrmNode.value.rot.writeln;
-		}
+		////foreach (wyrmNode; wyrms.iterator) {
+		////	import std.stdio;
+		////	wyrmNode.value.rot.writeln;
+		////}
 	}
 	
 	GameEvent[] handleInput() {
 		GameEvent gameEvent;
 		{
-			gameEvent.type = EventType.wyrmRotate;
-			gameEvent.wyrmRotate.torque = -3;
+			gameEvent.type = EventType.thrust;
+			gameEvent.thrust.thrust = -3;
 		}
 		return [gameEvent];
 	}
