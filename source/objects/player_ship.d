e54509c8e95c5bf70166fbf74a736c86f01bc91a
Unknown <JonathanILevi@gmail.com>
2018-05-08 20:41:07 -0500

    PlayerShip velocity vars

--- source/objects/player_ship.d
+++ source/objects/player_ship.d
@@ -3,6 +3,8 @@
 module objects.player_ship;
 
 class PlayerShip{
-	float[2]	pos = [0,0];
-	float	rot = 0;
+	float[2]	pos	= [0,0];
+	float	rot	= 0;
+	float[2]	velocity	= [0,0];
+	float	torque	= 0;
 }
