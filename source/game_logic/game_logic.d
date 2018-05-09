3cea1b358fc7d41b0e9aab8b74b72deb9981441e
Unknown <JonathanILevi@gmail.com>
2018-05-08 20:12:28 -0500

    `GameLogic` now adds 3 dumby ships.

--- source/game_logic/game_logic.d
+++ source/game_logic/game_logic.d
@@ -2,12 +2,17 @@ module game_logic.game_logic;
 
 import llist.slist;
 import objects.world;
+import objects.ship;
 import game_events.game_event;
+import math.tau;
 
 class GameLogic {
 	World world;
 	this(World world) {
 		this.world = world;
+		world.ships.append(new Ship([0,-3],0));
+		world.ships.insertAfter(new Ship([2,2],0.5*TAU));
+		world.ships.insertAfter(new Ship([-2,1],0.1*TAU));
 	}
 	
 	void update(GameEvent[] gameEvents) {
